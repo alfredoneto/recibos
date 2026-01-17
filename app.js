b4af2e570faee0eebcc517da52f9658a550c898b
alfredoneto <alfredodearaujo@gmail.com>
2026-01-17 15:20:50 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+let itens = [];
+let total = 0;
+
+function adicionarItem() {
+  const descricao = document.getElementById("descricao").value;
+  const valor = Number(document.getElementById("valor").value);
+
+  if (!descricao || !valor) return;
+
+  itens.push({ descricao, valor });
+  total += valor;
+
+  renderItens();
+}
+
+function renderItens() {
+  const ul = document.getElementById("listaItens");
+  ul.innerHTML = "";
+
+  itens.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = `${item.descricao} - R$ ${item.valor.toFixed(2)}`;
+    ul.appendChild(li);
+  });
+
+  document.getElementById("total").innerText = `Total: R$ ${total.toFixed(2)}`;
+}
+
+function gerarRecibo() {
+  const recibo = {
+    clienteNome: clienteNome.value,
+    clienteEndereco: clienteEndereco.value,
+    clienteDocumento: clienteDocumento.value,
+    itens,
+    total,
+    data: new Date()
+  };
+
+  localStorage.setItem("recibo", JSON.stringify(recibo));
+  window.location.href = "recibo.html";
+}
